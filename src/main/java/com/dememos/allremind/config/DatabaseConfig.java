6a0e9cf0e07c8a2fdd2f0c5d682c66be36e69bba
Dememos <dememos@mail.ru>
2016-03-15 17:51:31 +0300

    work the end time7 22

--- src/main/java/com/dememos/allremind/config/DatabaseConfig.java
+++ src/main/java/com/dememos/allremind/config/DatabaseConfig.java
@@ -1,5 +1,8 @@
 package com.dememos.allremind.config;
 
+import org.apache.commons.dbcp.BasicDataSource;
+import org.apache.commons.dbcp.managed.BasicManagedDataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -8,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.annotation.Resource;
+import javax.sql.DataSource;
 
 @Configuration
 @EnableJpaRepositories("com.dememos.allremind.repositories")
@@ -17,6 +21,18 @@ public class DatabaseConfig {
 
     @Resource
     private Environment env;
+
+    @Bean
+    public DataSource dataSource(){
+        BasicDataSource ds = new BasicDataSource();
+        ds.setUrl(env.getRequiredProperty("db.url"));
+        ds.setDriverClassName(env.getRequiredProperty("db.driver"));
+        ds.setUsername(env.getRequiredProperty("db.username"));
+        ds.setPassword(env.getRequiredProperty("db.password"));
+
+
+        return ds;
+    }
 
 
 }
